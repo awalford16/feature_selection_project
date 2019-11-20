d6f43850a0f4ed6c99fe4425329db86ab7c92447
awalford16 <awalford16@gmail.com>
2019-11-20 20:32:44 +0000

    change min max

--- feature_selection/filter_selection.py
+++ feature_selection/filter_selection.py
@@ -12,7 +12,7 @@ def pearson_correlation(data):
     # Plot matrix
     figure = plt.figure()
     ax = figure.add_subplot(111)
-    cax = ax.matshow(corr, vmin=0, vmax=1)
+    cax = ax.matshow(corr, vmin=-1, vmax=1)
     figure.colorbar(cax)
     ticks = np.arange(0,14,1)
     ax.set_xticks(ticks)
